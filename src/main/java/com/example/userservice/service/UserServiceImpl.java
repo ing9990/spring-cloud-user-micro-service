6fa34a1a7d030d8c4a0be1206d1fc17a90a0a3df
ing9990 <gimgau0218@naver.com>
2023-01-03 00:04:19 +0900

    develop: UserService를 상속받고 loadUserByUsername을 이메일 기준으로 구현

--- src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -6,10 +6,13 @@ import com.example.userservice.entity.UserEntity;
 import com.example.userservice.repository.UserRepository;
 import com.example.userservice.vo.RequestUser;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -44,6 +47,20 @@ public class UserServiceImpl implements UserService {
         return userRepository.findAll()
                 .stream().map(UserResponse::of)
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = userRepository.findUserEntityByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username + " 유저를 찾을 수 없습니다."));
+
+        return buildUserByEmailPassword(user);
+    }
+
+    private User buildUserByEmailPassword(UserEntity user) {
+        return new User(user.getEmail(), user.getEncryptedPassword(),
+                true, true, true, true,
+                new ArrayList<>());
     }
 }
 
