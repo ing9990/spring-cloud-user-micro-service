0e33e565aea80cc30d618043eb564a8dc6a937a5
ing9990 <gimgau0218@naver.com>
2023-01-03 00:03:39 +0900

    develop: Spring security의 기능을 사용하기 위해 UserDetailsService를 상속받음

--- src/main/java/com/example/userservice/service/UserService.java
+++ src/main/java/com/example/userservice/service/UserService.java
@@ -3,13 +3,16 @@ package com.example.userservice.service;
 import com.example.userservice.dto.UserRegisterDto;
 import com.example.userservice.dto.UserResponse;
 import com.example.userservice.vo.RequestUser;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 import java.util.List;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
+
     UserRegisterDto createUser(RequestUser requestUser);
 
     UserResponse getUserByUserId(String userId);
 
     List<UserResponse> getUserByAll();
+
 }
