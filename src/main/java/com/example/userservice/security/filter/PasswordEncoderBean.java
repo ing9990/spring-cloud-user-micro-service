7ed40df04305e7838fbea0e5e82a441935c4c4e1
ing9990 <gimgau0218@naver.com>
2023-01-03 00:01:48 +0900

    refactor: PasswordEncoder의 순환참조 방지를 위한 컴포넌트 클래스 추가

---
+++ src/main/java/com/example/userservice/security/filter/PasswordEncoderBean.java
@@ -0,0 +1,14 @@
+package com.example.userservice.security.filter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PasswordEncoderBean {
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
