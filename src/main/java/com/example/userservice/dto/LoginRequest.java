52aa23deefb4b89474620f1cda38fdf427d8c9a2
ing9990 <gimgau0218@naver.com>
2023-01-03 00:01:09 +0900

    develop: 로그인 요청을 받을 수 있는 LoginRequest 추가

---
+++ src/main/java/com/example/userservice/dto/LoginRequest.java
@@ -0,0 +1,27 @@
+package com.example.userservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LoginRequest {
+
+    @NotNull(message = "Email cannot be null")
+    @Size(min = 2, message = "Email not be less than two characters")
+    @Email
+    private String email;
+
+    @NotNull(message = "Password cannot be null")
+    @Size(min = 8, message = "Password not be greater than 8 characters")
+    private String password;
+
+}
