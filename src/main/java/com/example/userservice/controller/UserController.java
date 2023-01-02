29057ebf575e9108f426d66d507a205d4356ed50
ing9990 <gimgau0218@naver.com>
2023-01-03 00:02:27 +0900

    style: UserController의 엔드포인트에 user-service가 들어가지 않도록 변경

--- src/main/java/com/example/userservice/controller/UserController.java
+++ src/main/java/com/example/userservice/controller/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/user-service")
+@RequestMapping
 @RequiredArgsConstructor
 @Slf4j
 public class UserController {
