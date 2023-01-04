9b7b3edac172d62161ad33fbf9d7fe90d7ae63db
ing9990 <gimgau0218@naver.com>
2023-01-04 14:50:42 +0900

    style: 로그인 성공 주석 제거

--- src/main/java/com/example/userservice/security/filter/AuthenticationFilter.java
+++ src/main/java/com/example/userservice/security/filter/AuthenticationFilter.java
@@ -62,8 +62,6 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
         response.addHeader("token", token);
         response.addHeader("userId", userDetails.getUserId());
-
-        System.out.println("Login Success!!" + token);
     }
 }
 
